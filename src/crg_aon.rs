08954c96a18188964142eb8891ea9508a1743249
Harry Hornbacher <hhornbacher@rapitag.com>
2022-05-17 13:48:12 +0200

    Implement CrgAon register access

---
+++ src/crg_aon.rs
@@ -0,0 +1,24 @@
+use da14531::CRG_AON;
+
+pub trait CrgAonExt {
+    fn take(self) -> CrgAon;
+}
+
+impl CrgAonExt for CRG_AON {
+    fn take(self) -> CrgAon {
+        CrgAon
+    }
+}
+
+pub struct CrgAon;
+impl CrgAon {
+    const fn ptr() -> *const crate::pac::crg_aon::RegisterBlock {
+        crate::pac::CRG_AON::ptr()
+    }
+
+    pub fn set_pad_latch_en(&mut self, state: bool) {
+        unsafe {
+            (*CrgAon::ptr()).pad_latch_reg.write(|w| w.pad_latch_en().bit(state));
+        }
+    }
+}
